ee63e1a985bcf1808fca67f5bef52275f2f5f220
Rafaela <rafaelamunizsilva1@gmail.com>
2021-01-05 11:47:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+document.querySelector(".abas").addEventListener("click", () =>
+    document.querySelector(".sections").classList.toggle("show-notices")
+);
